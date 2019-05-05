a8c5a95f2e5b9b2a84b8a0b6326eb33831a910f2
Martin <stuurmartinpost@gmail.com>
2019-05-05 14:04:47 +0200

    setup folderstructure store and reducer

---
+++ src/actions/events.js
@@ -0,0 +1,22 @@
+import request from 'superagent'
+
+export const EVENTS_FETCHED = 'EVENTS_FETCHED'
+
+const baseUrl = 'http://localhost:4000'
+
+const eventsFetched = events => ({
+    type: EVENTS_FETCHED,
+    events
+})
+
+export const loadEvents = () => (dispatch, getState)=> {
+
+  if (getState().events) return
+
+  request (`${baseUrl}/events`)
+  .then(response => {
+
+    dispatch(eventsFetched(response.body))
+  })
+  .catch(console.error)
+}
